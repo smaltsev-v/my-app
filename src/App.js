dd1e8345419fe807e838411aa58db06bec5e2adb
Serhii Maltsev <83024347+smaltsev-v@users.noreply.github.com>
2021-12-07 21:00:39 +0200

    1
    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import PaintingList from './components/PaintingsList';
+import Section from './components/Section';
+import paintings from './paintings.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App() {
+  return(
+    <div>
+     <Section title="Топ недель" >
+        <PaintingList items={paintings} />
+      </Section>
+      
+      <Section title="Лучшее">
+        <PaintingList items={paintings} />
+      </Section>
+    
     </div>
-  );
+   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,96 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+
+ReactDOM.render(<App/>,
+  document.querySelector('#root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function Painting(props) {
+//   const { url, title, profileUrl, author, price } = props;
+
+//   return(
+//    <div>
+//      <img src={url} alt={title} width="480" />
+//      <h2>{title}</h2>
+//      <p>
+//         Автор: <a href={profileUrl}>{author}</a>
+//      </p>
+//      <p>Цена:{price} кредитов</p>
+//      <p>Доступность: заканчивается или есть в наличии</p>
+//      <button type="button">Добавить в корзину</button>
+//    </div>
+//  ); 
+//  }
+// const painting = paintings[2];
+// <Painting
+//     url={painting.url}
+//     title={painting.title}
+//     authorName={painting.author.tag}
+//     profileUrl={painting.author.url}
+//     price={painting.price }
+//   />,
+
+
+
+// const data = paintings[0];
+// const data2 = paintings[1];
+
+// // const elem1 = React.createElement('span', { children: 'Привет' });
+// // const elem2 = React.createElement('span', { children: 'мир' });
+
+// // JXS = использовать всегда 
+// const elem1 = <span>Привет</span>;
+
+
+// const elem2 = <span>мир</span>
+// const jxsElement = (
+//   <div>
+//     {elem1}
+//     {elem2}
+//   </div>
+// );
+
+
+// // const element = React.createElement('div', {
+// //   a: 5,
+// //   b: 10,
+// //   children: 'Привет мир',
+// // });
+// // const jxsElement = <div>Привет мир</div>;
+
+// console.log(jxsElement);
+// // ReactDOM.render(element,document.querySelector('#root'));
+//Создание компанента 
+// const painting1 = (
+
+// );
+// const painting2 = (
+// <div>
+//     <img src={data2.url} alt={data2.title} width="480" />
+//   <h2>{data2.title}</h2>
+//   <p>
+//       Автор:<a href={data2.author.url}>{data2.author.tag}</a>
+//   </p>
+//     <p>Цена: {data2.price} кредитов</p>
+//   <p>Доступность: заканчиваються или есть в наличии</p>
+//   <button type="button">Добавить в корзину</button>
+// </div>
+// );
+//
+// const data = {
+//   "id": "id-1",
+//   "url": "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
+//   "title": "Feathers. Art abstract",
+//   "price": 500,
+//   "author": {
+//     "tag": "ractapopulous",
+//     "url": "https://pixabay.com/users/ractapopulous-24766/"
+//   },
+//   "quantity": 10
+// };
+
+
